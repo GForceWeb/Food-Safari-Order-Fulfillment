be97472d686d52a950e9ed82695825fa1d4f41d6
Grant <grant@g-force.net>
2014-08-14 12:52:33 +0800

    Github repo change

--- foodsafari-fulfillment/foodsafari-fulfillment.php
+++ foodsafari-fulfillment/foodsafari-fulfillment.php
@@ -22,7 +22,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/<owner>/<repo>
+ * GitHub Plugin URI: https://github.com/GForceWeb/Food-Safari-Order-Fulfillment/
  * WordPress-Plugin-Boilerplate: v2.6.1
  */
 
